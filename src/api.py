093dce0232c8b98961f0b0af6287209602506cd8
Yudong-Fan <YF970919@hotmail.com>
2023-04-14 07:41:17 +0200

    jwt auth implemented

--- src/api.py
+++ src/api.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 from jwtHandler import jwtHandler
+from base64 import urlsafe_b64decode
+import json
 
 app = Flask(__name__)
 cors = CORS(app) # cors is added in advance to allow cors requests
@@ -47,3 +49,17 @@ def validation_check():
         get_data=request.args
         get_dict = get_data.to_dict()
         token = get_dict['jwt']
+        encoded_header, encoded_payload, encoded_signature = token.split('.')
+        # decode the base64url encoded string into a byte string, then decode it into a string
+        # the payload if of the form of {"name": "<username>"}
+        payload = jwtHandler.decode_base64url(encoded_payload).decode()
+        # convert the payload string into a json object    
+        user_name = json.loads(payload)['name']
+        # decode the signature in the request into a byte string
+        signature = jwtHandler.decode_base64url(encoded_signature)
+        # generate the expected signature in the form of byte string based on the username
+        correct_signature = jwtHandler.generate_expected_signature(user_name)
+        if signature == correct_signature:
+            return {"message": "Authentication Successful", "name": user_name}, 200
+        else:
+            return {"message": "Authentication Failed", "signature": correct_signature}, 401
