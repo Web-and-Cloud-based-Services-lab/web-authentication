ec305cc22592f5b45a41ceac0b2b95d6b28c5d52
Yuna C <yunabell@outlook.com>
2023-04-22 16:17:45 +0200

    speficy db client role

--- src/dbClient.py
+++ src/dbClient.py
@@ -1,4 +1,7 @@
 # settings of mongoDB client
 import pymongo 
 
-mongo_client = pymongo.MongoClient('127.0.0.1',27017)
+username = 'user_auth'
+password = 'user_auth_password'
+
+mongo_client = pymongo.MongoClient('mongodb://{user}:{pwd}@localhost:27017/admin'.format(user = username, pwd = password))
